4cde3ca8213515565cc7b80082445a35aae97f74
brunoprinz <brunosolsame2@gmail.com>
2025-12-25 20:41:14 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -215,15 +215,9 @@ const App: React.FC = () => {
                 <NavItem view={AppView.ACADEMY} icon={BookOpen} label={t.academy} />
                 <NavItem view={AppView.EXTRA_INCOME} icon={Coins} label={t.extra} />
                 
-                //<div className="pt-4 border-t border-slate-800 mt-4">
-                  //<button 
-                    //onClick={startTour}
-                    //className="w-full flex items-center gap-3 px-4 py-3 rounded-lg text-slate-400 hover:bg-slate-800 hover:text-white transition-all"
-                  //>
-                   // <HelpCircle className="w-5 h-5" />
-                   // <span className="font-medium">Help Tour</span>
-                 // </button>
-                //</div>
+                <div className="pt-4 border-t border-slate-800 mt-4">
+              
+                </div>
             </nav>
           </div>
         )}
